2b0aae0dff5ea28dd19cab6274110bc23027d6d4
Nook <nook@harquail.com>
2014-09-18 23:19:38 -0400

    testing env with heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 var Parse = require('parse').Parse;
 
-Parse.initialize(process.env.PARSE_APP_ID,process.env.PARSE_SECRET);
+Parse.initialize(PARSE_APP_ID,PARSE_SECRET);
 // Parse.initialize('3agAESVWJ46UPAG6wGuh0utwQe5pm3reUeWvbw0L','xDMSVh5xuIgJ26EDF1R3uHZ9tEc8fOlaU78uowPR');
 
 
