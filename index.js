c5747f44e8a6f1b361a2cfde4c70d042c8429013
Nook <nook@harquail.com>
2014-09-18 21:19:55 -0400

    there

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+app.post('/', function(req,res){
+	
+	var text = req.body.text;
+	if( text =='help') {		
+		res.send('/lights commands: on, off, random, very dim, dim, shady, bright, very bright, blue, green, orange, purple, normal, colors, sauron, party, pulse, pulses tv');	
+	}
+	else{
+		res.send('RAWR');	
+	}
+	
+});
