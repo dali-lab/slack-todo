a5fa01f109e4311a8b8242e232e724fddd9c5e4a
Nook <nook@harquail.com>
2014-09-18 23:09:34 -0400

    added env var

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var Parse = require('parse').Parse;
 
 // Parse.initialize(process.env.PARSE_APP_ID,process.env.PARSE_SECRET);
-Parse.initialize(3agAESVWJ46UPAG6wGuh0utwQe5pm3reUeWvbw0L,DMSVh5xuIgJ26EDF1R3uHZ9tEc8fOlaU78uowPR);
+Parse.initialize('3agAESVWJ46UPAG6wGuh0utwQe5pm3reUeWvbw0L','DMSVh5xuIgJ26EDF1R3uHZ9tEc8fOlaU78uowPR');
 
 var TestObject = Parse.Object.extend("TestObject");
 var testObject = new TestObject();
