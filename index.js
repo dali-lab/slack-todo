350667e42127b1a2b6b880f656d571f75e40f08a
Nook <nook@harquail.com>
2014-09-18 22:41:01 -0400

    added env var

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var express = require('express')
 var app = express();
 
+var Parse = require('parse').Parse;
+
+Parse.initialize(process.env.PARSE_APP_ID,process.env.PARSE_SECRET);
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
+
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use( bodyParser.urlencoded() ); // to support URL-encoded bodies
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
