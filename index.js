e925c59ffb9d1df48b244822bb8059852bee3f30
Nook <nook@harquail.com>
2014-09-18 23:23:57 -0400

    testing env with heroku

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ var app = express();
 
 var Parse = require('parse').Parse;
 
-Parse.initialize(PARSE_APP_ID,PARSE_SECRET);
+Parse.initialize(process.env.PARSE_APP_ID,process.env.PARSE_SECRET);
 // Parse.initialize('3agAESVWJ46UPAG6wGuh0utwQe5pm3reUeWvbw0L','xDMSVh5xuIgJ26EDF1R3uHZ9tEc8fOlaU78uowPR');
-
-
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
