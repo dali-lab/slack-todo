085cd06c48478227d3e675eadb0b0439fe57f1d3
Nook <nook@harquail.com>
2014-09-18 21:31:44 -0400

    added body-parser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express();
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use( bodyParser.urlencoded() ); // to support URL-encoded bodies
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
