052754e1c7f741b9a997298fb71886851ea11c9b
Nook <nook@harquail.com>
2014-09-19 23:49:57 -0400

    added collection for list

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 var express = require('express')
 var app = express();
 
+//parse nonsense
 var Parse = require('parse').Parse;
-
 Parse.initialize(process.env.PARSE_APP_ID,process.env.PARSE_SECRET);
-
-
-
-
-
-
 
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -28,38 +22,45 @@ app.listen(app.get('port'), function() {
 })
 
 app.post('/', function(req,res){
-	
-	
+		
 	var user = req.body.user_id;
 	var text = req.body.text;
 
-	// var query = new Parse.Query(TestObject);
-// 	query.equalTo("username", user);
-	
- 	
- 
-	
-	// var TestObject = Parse.Object.extend("Tester");
-	// var testObject = new TestObject();
-	// testObject.save({foo: "baretto"}).then(function(object) {
-	//   alert("yay! it worked");
-	// });
-	
+
 	if( text =='help') {		
-		res.send('/lights commands: on, off, random, very dim, dim, shady, bright, very bright, blue, green, orange, purple, normal, colors, sauron, party, pulse, pulses tv');	
+		res.send('not implemented');	
+	}
+	else if( text =='what') {		
+		var collection = new TestCollection();
+		collection.comparator = function(object) {
+		  return object.get("createdAt");
+		};
+		
+		var list = "";
+		for (i = 0; i < collection.length; i++) {
+			list += i;
+			list += '. ';
+			list += collection.at(i);
+			list += '\n';
+			 
+		}
+		res.send('RAWR');	
+		
 	}
 	else if( text =='user') {		
 		res.send(user);	
 	}
+	else if( text =='clear') {		
+		res.send(user);	
+	}
 	else{
- 	
+ 		//add a new list item with the message and username of the current command
 		var ListItem = Parse.Object.extend("ListItem");
 		var listItem = new ListItem();
 		listItem.save({
 		    username: user,
 		    message: text
 		  });
-	
 		res.send('RAWR');	
 	}
 	
